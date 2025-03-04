85e60d379aa05b2073309fef3a57bc5bea55c0db
Leeseoleem <leeseorim0029@gmail.com>
2025-03-04 23:48:03 +0900

    refactor env

--- mood-set/src/firebase.tsx
+++ mood-set/src/firebase.tsx
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBoAHfdATF4o7Ct5O2NK9lYq88JmAh6fDo",
-  authDomain: "mood-set-f2239.firebaseapp.com",
-  projectId: "mood-set-f2239",
-  storageBucket: "mood-set-f2239.firebasestorage.app",
-  messagingSenderId: "826542357279",
-  appId: "1:826542357279:web:54bdb71967a99461368e2a",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
